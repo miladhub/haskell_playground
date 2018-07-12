e0881f62da55199ef325ed3b6ea2947dc151e613
Milad Bourhani <milad.bourhani@gmail.com>
2018-07-12 23:38:07 +0200

    EitherT monad

--- eithert.hs
+++ eithert.hs
@@ -1,27 +1,29 @@
+module Either2 where
+
 newtype EitherT e m a =
   EitherT { runEitherT :: m (Either e a) }
-  
-instance Functor m
-      => Functor (EitherT e m) where
-  fmap f (EitherT mea) = EitherT $ (fmap . fmap) f mea
-  
-instance Applicative m
-      => Applicative (EitherT e m) where
+
+instance Functor m => Functor (EitherT e m) where
+  fmap f (EitherT mea) =
+    let meb = (fmap . fmap) f mea
+    in EitherT meb
+
+instance Applicative m => Applicative (EitherT e m) where
   pure a = EitherT $ pure (Right a)
-  (EitherT mef) <*> (EitherT mea) = EitherT $ fmap (<*>) mef <*> mea
-  
+  (EitherT emf) <*> (EitherT ema) = EitherT $ fmap (<*>) emf <*> ema
+
+instance Monad m => Monad (EitherT e m) where
+  return = pure
+  (EitherT ema) >>= f = EitherT $ do
+    ea <- ema
+    case ea of
+      Left e -> return (Left e)
+      Right a -> runEitherT (f a)
+
 {-
-  mef = m (Either e a->b)
-  mea = m (Either e a)
-  (<*>) (Either e a->b) = (Either e a) -> (Either e b)
-  fmap (<*>) (m (Either e a->b)) = m ( (Either e a) -> (Either e b) )
-  fmap (<*>) mef = m ( (Either e a) -> (Either e b) )
-  m ( (Either e a) -> (Either e b) ) <*> m (Either e a) = m (Either e b)
-  fmap (<*>) mef <*> mea
+let e = EitherT $ [Right 1, Left "foo"]
+let f = EitherT $ [Right (+1)]
+runEitherT $ f <*> e
 -}
-{-
-*Main> let l = [Right (+1)]
-*Main> let e = EitherT l
-*Main> runEitherT $ e <*> EitherT [Right 1]
-[Right 2]
--}
+
+
