d926ef5ee38ccce349e9d02c92ddfa74a582a65a
Milad Bourhani <milad.bourhani@gmail.com>
2018-07-14 22:23:45 +0200

    either t

--- eithert.hs
+++ eithert.hs
@@ -24,6 +24,17 @@ instance Monad m => Monad (EitherT e m) where
 let e = EitherT $ [Right 1, Left "foo"]
 let f = EitherT $ [Right (+1)]
 runEitherT $ f <*> e
+et = EitherT $ return $ Right (42 :: Int) :: EitherT String IO Int
 -}
 
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT mea) = EitherT $ fmap swapEither mea
 
+swapEither :: Either e a -> Either a e
+swapEither (Left e) = Right e
+swapEither (Right a) = Left a
+
+eitherT :: Monad m => (e -> m c) -> (a -> m c) -> EitherT e m a -> m c
+eitherT emc amc (EitherT mea) = do
+  ea <- mea
+  either emc amc ea
