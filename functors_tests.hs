7b3862ae1b3e9509491167f578df1dc3059ac141
Milad Bourhani <milad.bourhani@gmail.com>
2018-09-04 21:51:22 +0200

    more functors

--- functors_tests.hs
+++ functors_tests.hs
@@ -26,3 +26,77 @@ instance Arbitrary a => Arbitrary (Identity a) where
 
 type IdentityIntFC = Identity Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
 -- quickCheck (functorCompose :: IdentityIntFC)
+
+data Pair a = Pair a a
+  deriving (Show, Eq)
+
+instance Functor Pair where
+  fmap f (Pair a' a) = Pair (f a') (f a)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = Pair <$> arbitrary <*> arbitrary
+
+type PairIntFC = Pair Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
+-- quickCheck (functorCompose :: PairIntFC)
+
+data Two a b = Two a b
+  deriving (Show, Eq)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = Two <$> arbitrary <*> arbitrary
+
+type TwoIntFC = Two Int Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
+-- quickCheck (functorCompose :: TwoIntFC)
+
+data Three a b c = Three a b c
+  deriving (Show, Eq)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = Three <$> arbitrary <*> arbitrary <*> arbitrary
+
+type ThreeIntFC = Three Int Int Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
+-- quickCheck (functorCompose :: ThreeIntFC)
+
+data Three' a b = Three' a b b
+  deriving (Show, Eq)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b' b'') = Three' a (f b') (f b'')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = Three' <$> arbitrary <*> arbitrary <*> arbitrary
+
+type ThreeIntFC' = Three' String Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
+-- quickCheck (functorCompose :: ThreeIntFC')
+
+data Four a b c d = Four a b c d
+  deriving (Show, Eq)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = Four <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+type FourIntFC = Four String String String Int -> (Fun Int String) -> (Fun String Int) -> Bool
+-- quickCheck (functorCompose :: FourIntFC)
+
+data Four' a b = Four' a a a b
+  deriving (Show, Eq)
+
+instance Functor (Four' a) where
+  fmap f (Four' a' a'' a''' b) = Four' a' a'' a''' (f b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = Four' <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+type FourIntFC' = Four' String Int -> (Fun Int Int) -> (Fun Int Int) -> Bool
+-- quickCheck (functorCompose :: FourIntFC')
+
+
