fa24ea0f71c1fdfbf17969661ff6f25d0c2f8243
Milad Bourhani <milad.bourhani@gmail.com>
2018-09-05 08:12:43 +0200

    more functors

--- functors_tests.hs
+++ functors_tests.hs
@@ -108,3 +108,12 @@ instance Functor Possibly where
   fmap f (Yeppers a) = Yeppers $ f a
   fmap _ LolNope = LolNope
 
+data Sum a b =
+  First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap f (Second b) = Second $ f b
+  fmap _ (First a) = (First a)
+
