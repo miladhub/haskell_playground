03d06f66be41d1c28fd3ad2fc4fa2af87b112eb7
Milad Bourhani <milad.bourhani@gmail.com>
2018-10-08 21:49:38 +0200

    zip applicative

--- ziplist_applicative.hs
+++ ziplist_applicative.hs
@@ -1,19 +1,32 @@
 module MyZipListApplicative where
 
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 data List a =
   Nil
   | Cons a (List a)
   deriving (Eq, Show)
 
 take' :: Int -> List a -> List a
-take' = undefined
+take' _ Nil = Nil
+take' n (Cons a t) = Cons a (take' (n - 1) t)
 
 instance Functor List where
-  fmap = undefined
+  fmap _ Nil = Nil
+  fmap f (Cons a l) = (Cons $ f a) $ fmap f l
 
 instance Applicative List where
-  pure = undefined
-  (<*>) = undefined
+  pure a = Cons a Nil
+  Nil <*> _ = Nil
+  (Cons f t) <*> l = (fmap f l) <> (t <*> l)
+
+instance Monoid (List a) where
+  mempty = Nil
+  mappend Nil l = l
+  mappend (Cons a t) l = Cons a (mappend t l)
 
 newtype ZipList' a =
   ZipList' (List a)
@@ -31,5 +44,26 @@ instance Functor ZipList' where
     ZipList' $ fmap f xs
 
 instance Applicative ZipList' where
-  pure = undefined
-  (<*>) = undefined
+  pure = ZipList' <$> pure
+  ZipList' Nil <*> _ = ZipList' Nil
+  _ <*> ZipList' Nil = ZipList' Nil
+  ZipList' (Cons f fs) <*> ZipList' (Cons a as) =
+    ZipList' ( Cons (f a) ((ZipList' fs) <*> (ZipList' as)) )
+
+--  pure = ZipList' <$> pure
+--  (ZipList' lf) <*> (ZipList' l) = ZipList' $ lf <*> l
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary =
+    frequency [
+      (1, return Nil),
+      (1, Cons <$> arbitrary <*> arbitrary)
+    ]
+
+instance Arbitrary a => Arbitrary (ZipList' a) where
+  arbitrary = ZipList' <$> arbitrary
+
+main :: IO ()
+main =
+  let xs = ZipList' $ Cons ("b", "w", 1 :: Int) Nil
+  in quickBatch $ applicative xs
