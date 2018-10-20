862735177f93649eeb8a4c8793997ae9e11096a1
Milad Bourhani <milad.bourhani@gmail.com>
2018-10-20 21:06:18 +0200

    combinations with liftA3

---
+++ combinations.hs
@@ -0,0 +1,17 @@
+module Combs where
+
+import Control.Applicative (liftA3)
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos = liftA3 (,,)
+
+{-
+ - combos stops vowels stops 
+-}
+
