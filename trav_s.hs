1e17cb808a8aa58fe17a22d91da1b6d72ed64bfa
Milad Bourhani <milad.bourhani@gmail.com>
2018-11-25 23:01:14 +0100

    traversable

--- trav_s.hs
+++ trav_s.hs
@@ -2,6 +2,7 @@
 
 module SkiFree where
 
+import Data.Monoid
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
@@ -13,24 +14,33 @@ data S n a =
 instance Functor n => Functor (S n) where
   fmap f (S na a) = S (f <$> na) (f a)
 
-instance Foldable (S n) where
-  foldr f z (S na a) = f a z
+instance Foldable n => Foldable (S n) where
+  foldMap f (S n a) = foldMap f n <> f a
 
 instance ( Functor n, Arbitrary (n a), Arbitrary a ) => Arbitrary (S n a) where
   arbitrary =
     S <$> arbitrary <*> arbitrary
 
+instance (Eq a, Eq (n a)) => EqProp (S n a) where
+  (=-=) = eq
+
+{-
+This doesn't work.
 instance ( Applicative n, Testable (n Property), EqProp a ) => EqProp (S n a) where
   (S x y) =-= (S p q) =
     (property $ (=-=) <$> x <*> p) .&. (y =-= q)
+-}
 
 instance Traversable n => Traversable (S n) where
-  traverse = undefined
+  traverse f (S na a) =
+    let fnb = traverse f na
+        fb = f a
+        snb = S <$> fnb <*> fb
+    in snb
 
 main :: IO ()
 main =
   let xs = undefined :: S [] (Int, Int, [Int])
   in do
-    --sample' (arbitrary :: Gen (S [] Int))
     quickBatch $ functor xs
-    --quickBatch $ traversable xs
+    quickBatch $ traversable xs
