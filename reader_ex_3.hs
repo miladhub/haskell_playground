109a66665da4c00f6742ac7c3b4f1399be619653
Milad Bourhani <milad.bourhani@gmail.com>
2018-12-03 22:11:40 +0100

    reader

--- reader_ex_3.hs
+++ reader_ex_3.hs
@@ -14,3 +14,32 @@ mylookup a abs =
   where
     match a (a', b) | a == a'   = Just b
                     | otherwise = Nothing
+                    
+-- zip x and y using 3 as the lookup key
+xs :: Maybe Integer
+xs = lookup 3 $ x `zip` y
+
+-- zip y and z using 6 as the lookup key
+ys :: Maybe Integer 
+ys = lookup 6 $ y `zip` z
+
+
+-- it's also nice to have one that
+-- will return Nothing, like this one
+-- zip x and y using 4 as the lookup key
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+-- now zip x and z using a
+-- variable lookup key
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ x `zip` z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer) 
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = (,) <$> z' <*> z'
