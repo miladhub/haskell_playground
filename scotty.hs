acb84eba1c5928c848f66af6567e2dfc0db4b8f6
milad.bourhani <milad.bourhani@accenture.com>
2019-04-10 17:34:17 +0200

    scotty

--- scotty.hs
+++ scotty.hs
@@ -4,11 +4,29 @@ module Scotty where
 
 import Web.Scotty
 import Data.Monoid (mconcat)
+import Web.Scotty.Internal.Types (ActionT(..))
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Lazy hiding (get)
+
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT m = ReaderT (const m)
+
+liftStateT :: Monad m => m a -> StateT s m a
+liftStateT m = StateT $ \s -> do
+  a <- m
+  return (a, s)
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    lift (putStrLn "hello")
+    --lift (putStrLn "hello")
+    (ActionT
+      . (ExceptT . liftM Right)
+      . liftReaderT
+      . liftStateT
+      ) (putStrLn "hello")
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
 
