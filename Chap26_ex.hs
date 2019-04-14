d249c22cf00eb9592706b10e5eba21f0fac8d402
Milad Bourhani <milad.bourhani@gmail.com>
2019-04-14 13:00:43 +0200

    scotty

--- Chap26_ex.hs
+++ Chap26_ex.hs
@@ -3,6 +3,9 @@ module Chap26Ex where
 import Control.Monad.Trans.Reader
 import Data.Functor.Identity
 import Control.Monad.Trans.State.Lazy
+import Control.Monad.Trans.Maybe
+import Control.Monad
+import Control.Monad.IO.Class
 
 rDec :: Num a => Reader a a
 rDec = ReaderT $ Identity . ((-) 1)
@@ -18,3 +21,22 @@ rPrintAndInc = ReaderT $ \a -> do
 sPrintIncAccum :: (Num a, Show a) => StateT a IO String
 sPrintIncAccum = StateT $ \a -> do
   return (show a, a + 1)
+
+isValid :: String -> Bool
+isValid v = '!' `elem` v
+
+maybeExcite :: MaybeT IO String
+maybeExcite = do
+  v <- liftIO $ getLine
+  guard $ isValid v
+  return v
+
+doExcite :: IO ()
+doExcite = do
+  putStrLn "say something excite!"
+  excite <- runMaybeT maybeExcite
+  case excite of
+    Nothing -> putStrLn "MOAR EXCITE"
+    Just e ->
+      putStrLn
+        ("Good, was very excite: " ++ e)
