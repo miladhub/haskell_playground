de17138a5e03587c7d0ed8f8e1c15b9bb45cd012
milad.bourhani <milad.bourhani@accenture.com>
2019-04-19 16:54:52 +0200

    refactored

--- morra.hs
+++ morra.hs
@@ -8,37 +8,44 @@ import Control.Monad.IO.Class
 import System.Random
 
 data Winner =
-  P | C
+  Player | Computer
   deriving (Eq, Show)
 
 data Scores =
   Scores {
-    p :: Integer,
-    c :: Integer
+    player   :: Integer
+  , computer :: Integer
   }
   deriving (Eq, Show)
+
+type Game a = StateT Scores IO a
+type Turn a = StateT Scores IO (Maybe a)
 
 main :: IO ()
 main = do
   putStrLn "-- P is Player"
   putStrLn "-- C is Computer"
   putStrLn "-- Player is odds, computer is evens."
-  (w, s) <- runStateT game $ Scores 0 0
-  putStrLn $ "Final score " ++ (show s)
-  putStrLn $ "Winner " ++ (show w)
+  winner <- run
+  putStrLn $ "Contratulations to " ++ (show winner) ++ "!"
 
-game :: StateT Scores IO Winner
+run :: IO Winner
+run = do
+  (winner, _) <- runStateT game (Scores 0 0)
+  return winner
+
+game :: Game Winner
 game = do
   mw <- turn
   case mw of
     Nothing -> game
     Just w  -> return w
 
-turn :: StateT Scores IO (Maybe Winner)
+turn :: Turn Winner
 turn = StateT $ \s -> do
   w <- throw
   let newScore = scores s w
-  if (p newScore) == 3 || (c newScore) == 3 then
+  if (player newScore) == 3 || (computer newScore) == 3 then
     return (Just w, newScore)
   else
     return (Nothing, newScore)
@@ -49,13 +56,13 @@ throw = do
   p <- readLn
   c <- oneOrTwo
   putStrLn $ "C: " ++ (show c)
-  let winner = if (p + c) `mod` 2 == 0 then C else P
+  let winner = if (p + c) `mod` 2 == 0 then Computer else Player
   putStrLn $ "- " ++ (show winner) ++ " wins"
   return winner
 
 scores :: Scores -> Winner -> Scores
-scores s P = Scores ( (p s) + 1 ) (c s)
-scores s C = Scores (p s) ( (c s) + 1 )
+scores s Player   = Scores ( (player s) + 1 ) (computer s)
+scores s Computer = Scores (player s) ( (computer s) + 1 )
 
 oneOrTwo :: IO Integer
-oneOrTwo = getStdRandom (randomR (1,2))
+oneOrTwo = getStdRandom (randomR (1, 2))
